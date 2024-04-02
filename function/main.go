493a959160f8b98d4a2dd7ba2d56085300097c16
Qizhao Chen <kx412764776@gmail.com>
2024-04-02 17:54:36 +0100

    function

--- function/main.go
+++ function/main.go
@@ -3,21 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	sum(5)
+
+	sum := sum(5, 10)
+	fmt.Println(sum)
+
+	res := cumulativeSum(5)
+	fmt.Println(res)
 
 	traverseString("Hello golang")
+	traverseStringWithForRange("Hello golang")
+
 }
 
-func sum(num int) {
+func sum(n1 int, n2 int) int {
+	return n1 + n2
+}
+
+func cumulativeSum(num int) int {
 	var res = num
 	for i := 0; i <= num; i++ {
 		res += i
 	}
-	fmt.Println(res)
+	return res
 }
 
 func traverseString(s string) {
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("%c", s[i])
+	}
+	fmt.Println()
+}
+
+func traverseStringWithForRange(s string) {
+	for i, value := range s {
+		fmt.Printf("%c index: %d\n", value, i)
 	}
 }
